f02ee077f4442afc716e7480f307b9302bd6ca12
mtfishman <mfishman@flatironinstitute.org>
2025-01-09 15:37:02 -0500

    Try fixing tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,5 +3,5 @@ using Aqua: Aqua
 using Test: @testset
 
 @testset "Code quality (Aqua.jl)" begin
-  Aqua.test_all(ITensorBase; ambiguities=false)
+  Aqua.test_all(ITensorBase; ambiguities=false, persistent_tasks=false)
 end
