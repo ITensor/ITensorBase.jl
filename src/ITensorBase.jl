bb400c713ebc8c4dd1a3333d338bb35f82e48cb2
mtfishman <mfishman@flatironinstitute.org>
2025-01-12 09:40:51 -0500

    Printing

--- src/ITensorBase.jl
+++ src/ITensorBase.jl
@@ -23,20 +23,42 @@ const TagSet = Set{Tag}
 
 tagset(tags::String) = Set(filter(!isempty, String.(strip.(split(tags, ",")))))
 
+function tagsstring(tags::TagSet)
+  str = ""
+  length(tags) == 0 && return str
+  tags_vec = collect(tags)
+  for n in 1:(length(tags_vec) - 1)
+    str *= "$(tags_vec[n]),"
+  end
+  str *= "$(tags_vec[end])"
+  return str
+end
+
 @kwdef struct IndexName <: AbstractName
   id::UInt64 = rand(UInt64)
-  plev::Int = 0
   tags::TagSet = TagSet()
+  plev::Int = 0
 end
 NamedDimsArrays.randname(n::IndexName) = IndexName()
 
+id(n::IndexName) = n.id
 tags(n::IndexName) = n.tags
+plev(n::IndexName) = n.plev
+
 settags(n::IndexName, tags) = @set n.tags = tags
 addtags(n::IndexName, ts) = settags(n, tags(n) ∪ tagset(ts))
 
-plev(n::IndexName) = n.plev
 setprime(n::IndexName, plev) = @set n.plev = plev
 prime(n::IndexName) = setprime(n, plev(n) + 1)
+
+function Base.show(io::IO, i::IndexName)
+  idstr = "id=$(id(i) % 1000)"
+  tagsstr = !isempty(tags(i)) ? "|\"$(tagsstring(tags(i)))\"" : ""
+  primestr = primestring(plev(i))
+  str = "IndexName($(idstr)$(tagsstr))$(primestr)"
+  print(io, str)
+  return nothing
+end
 
 struct IndexVal{Value<:Integer} <: AbstractNamedInteger{Value,IndexName}
   value::Value
@@ -60,7 +82,12 @@ function Index(length::Int, tags::String; kwargs...)
   return Index(Base.OneTo(length), IndexName(; kwargs..., tags=tagset(tags)))
 end
 
+# TODO: Define for `NamedDimsArrays.NamedViewIndex`.
+id(i::Index) = id(name(i))
 tags(i::Index) = tags(name(i))
+plev(i::Index) = plev(name(i))
+
+# TODO: Define for `NamedDimsArrays.NamedViewIndex`.
 addtags(i::Index, tags) = setname(i, addtags(name(i), tags))
 prime(i::Index) = setname(i, prime(name(i)))
 Base.adjoint(i::Index) = prime(i)
@@ -72,6 +99,29 @@ NamedDimsArrays.name(i::Index) = i.name
 
 # Constructor
 NamedDimsArrays.named(i::AbstractUnitRange, name::IndexName) = Index(i, name)
+
+function primestring(plev)
+  if plev < 0
+    return " (warning: prime level $plev is less than 0)"
+  end
+  if plev == 0
+    return ""
+  elseif plev > 3
+    return "'$plev"
+  else
+    return "'"^plev
+  end
+end
+
+function Base.show(io::IO, i::Index)
+  lenstr = "length=$(dename(length(i)))"
+  idstr = "|id=$(id(i) % 1000)"
+  tagsstr = !isempty(tags(i)) ? "|\"$(tagsstring(tags(i)))\"" : ""
+  primestr = primestring(plev(i))
+  str = "Index($(lenstr)$(idstr)$(tagsstr))$(primestr)"
+  print(io, str)
+  return nothing
+end
 
 struct NoncontiguousIndex{T,Value<:AbstractVector{T}} <:
        AbstractNamedVector{T,Value,IndexName}
--- src/quirks.jl
+++ src/quirks.jl
@@ -1,4 +1,12 @@
 # TODO: Define this properly.
 dag(i::Index) = i
+# TODO: Deprecate.
+dim(i::Index) = dename(length(i))
+# TODO: Define this properly.
 hasqns(i::Index) = false
-dim(i::Index) = length(i)
+inds(a::ITensor) = nameddimsindices(a)
+# TODO: Deprecate.
+itensor(parent::AbstractArray, nameddimsindices) = ITensor(parent, nameddimsindices)
+function itensor(parent::AbstractArray, i1::Index, i_rest::Index...)
+  return ITensor(parent, (i1, i_rest...))
+end
