08603d35cd2316aa49fe35ab6fd253c945cf17b9
mtfishman <mfishman@flatironinstitute.org>
2025-01-17 09:43:49 -0500

    Format

--- src/quirks.jl
+++ src/quirks.jl
@@ -1,7 +1,9 @@
 # TODO: Define this properly.
 dag(i::Index) = i
+dag(a::ITensor) = a
 # TODO: Deprecate.
 dim(i::Index) = dename(length(i))
+dim(a::AbstractITensor) = unname(length(a))
 # TODO: Define this properly.
 hasqns(i::Index) = false
 # TODO: Deprecate.
@@ -9,6 +11,8 @@ itensor(parent::AbstractArray, nameddimsindices) = ITensor(parent, nameddimsindi
 function itensor(parent::AbstractArray, i1::Index, i_rest::Index...)
   return ITensor(parent, (i1, i_rest...))
 end
+# TODO: Deprecate.
+order(a::AbstractArray) = ndims(a)
 
 # This seems to be needed to get broadcasting working.
 # TODO: Investigate this and see if we can get rid of it.
@@ -23,9 +27,25 @@ function onehot(iv::Pair{<:Index,<:Int})
   return a
 end
 
-using LinearAlgebra: svd
+using LinearAlgebra: qr, svd
 # TODO: Define this in `MatrixAlgebra.jl`/`TensorAlgebra.jl`.
-function factorize(a::AbstractITensor, args...; kwargs...)
-  U, S, V = svd(a, args...; kwargs...)
-  return U, S * V
+function factorize(a::AbstractITensor, args...; tags=TagSet(), maxdim=nothing)
+  if isnothing(maxdim)
+    # `tags` are ignored right now.
+    Q, R = qr(a, args...)
+    return Q, R
+  else
+    error("Truncation in `factorize` not implemented yet.")
+    U, S, V = svd(a, args...; kwargs...)
+    return U, S * V
+  end
 end
+
+# TODO: Used in `ITensorMPS.jl`, decide where or if to define it.
+# Ideally this would just be a zero-dimensional `ITensor` wrapping
+# a special type, like `Zeros{UnspecifiedZero()}()`.
+struct OneITensor <: AbstractITensor end
+Base.size(::OneITensor) = ()
+Base.:*(::OneITensor, ::OneITensor) = OneITensor()
+Base.:*(::OneITensor, a::ITensor) = a
+Base.:*(a::ITensor, ::OneITensor) = a
