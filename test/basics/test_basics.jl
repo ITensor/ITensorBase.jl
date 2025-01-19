7a96d3719fbdc04acd492ea144949cc2e2f29618
Matt Fishman <mtfishman@users.noreply.github.com>
2025-01-19 17:24:54 -0500

    Reorganize into files (#19)

--- test/basics/test_basics.jl
+++ test/basics/test_basics.jl
@@ -1,6 +1,15 @@
-using ITensorBase: ITensorBase
+using ITensorBase: ITensorBase, ITensor, Index, inds, plev, prime
+using NamedDimsArrays: unname
 using Test: @test, @testset
 
 @testset "ITensorBase" begin
-  # Tests go here.
+  i, j = Index.((2, 2))
+  x = randn(2, 2)
+  a = ITensor(x, (i, j))
+  @test unname(a) == x
+  @test plev(i) == 0
+  @test plev(prime(i)) == 1
+  a′ = prime(a)
+  @test unname(a′) == x
+  @test issetequal(inds(a′), (prime(i), prime(j)))
 end
