04d71a7f7a95c5fc1817845d3766a7fc3337aa6c
mtfishman <mfishman@flatironinstitute.org>
2025-01-30 12:53:57 -0500

    Generalize Index constructor to allow ranges, define `hastag`

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -1,6 +1,7 @@
-using ITensorBase: ITensorBase, ITensor, Index, gettag, inds, plev, prime, settag, unsettag
+using ITensorBase:
+  ITensorBase, ITensor, Index, gettag, hastag, inds, plev, prime, settag, tags, unsettag
 using DiagonalArrays: δ, delta, diagview
-using NamedDimsArrays: unname
+using NamedDimsArrays: dename, name, named
 using Test: @test, @test_broken, @testset
 
 @testset "ITensorBase" begin
@@ -8,19 +9,31 @@ using Test: @test, @test_broken, @testset
     i, j = Index.((2, 2))
     x = randn(2, 2)
     for a in (ITensor(x, i, j), ITensor(x, (i, j)))
-      @test unname(a) == x
+      @test dename(a) == x
       @test plev(i) == 0
       @test plev(prime(i)) == 1
+      @test length(tags(i)) == 0
       a′ = prime(a)
-      @test unname(a′) == x
+      @test dename(a′) == x
       @test issetequal(inds(a′), (prime(i), prime(j)))
     end
 
     i = Index(2)
     i = settag(i, "X", "x")
+    @test hastag(i, "X")
+    @test !hastag(i, "Y")
     @test gettag(i, "X") == "x"
     i = unsettag(i, "X")
     @test isnothing(gettag(i, "X", nothing))
+    @test !hastag(i, "X")
+    @test !hastag(i, "Y")
+
+    i = Index(Base.OneTo(2))
+    @test length(i) == named(2, name(i))
+    @test dename(length(i)) == 2
+    @test dename(i) == 1:2
+    @test plev(i) == 0
+    @test length(tags(i)) == 0
   end
   @testset "delta" begin
     i, j = Index.((2, 2))
@@ -37,7 +50,7 @@ using Test: @test, @test_broken, @testset
       @test eltype(a) === Bool
       # TODO: Fix this.
       @test_broken diagview(a)
-      @test diagview(unname(a)) == ones(2)
+      @test diagview(dename(a)) == ones(2)
     end
   end
 end
