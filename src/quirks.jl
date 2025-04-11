f03d8276655b27322eae98f934d0cfb410b8c149
mtfishman <mfishman@flatironinstitute.org>
2025-04-11 18:23:01 -0400

    Fix for Julia 1.10

--- src/quirks.jl
+++ src/quirks.jl
@@ -20,6 +20,11 @@ hasqns(a::AbstractITensor) = all(hasqns, inds(a))
 # TODO: Investigate this and see if we can get rid of it.
 Base.Broadcast.extrude(a::AbstractITensor) = a
 
+# See: https://github.com/JuliaLang/julia/blob/v1.11.4/base/namedtuple.jl#L269
+# `filter(f, ::NamedTuple)` is available in Julia v1.11, delete once
+# we drop support for Julia v1.10.
+filter_namedtuple(f, xs::NamedTuple) = xs[filter(k -> f(xs[k]), keys(xs))]
+
 function translate_factorize_kwargs(;
   # MatrixAlgebraKit.jl/TensorAlgebra.jl kwargs.
   orth=nothing,
@@ -35,7 +40,7 @@ function translate_factorize_kwargs(;
   rtol = @something rtol cutoff Some(nothing)
   maxrank = @something maxrank maxdim Some(nothing)
   !isnothing(maxrank) && error("`maxrank` not supported yet.")
-  return filter(!isnothing, (; orth, rtol, maxrank, kwargs...))
+  return filter_namedtuple(!isnothing, (; orth, rtol, maxrank, kwargs...))
 end
 
 using TensorAlgebra: TensorAlgebra, factorize
